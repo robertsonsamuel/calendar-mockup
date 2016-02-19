48800116d2fb8c622bc292e0e51ac37dae67d0e5
Samuel Robertson <samuel.robertson93@gmail.com>
2016-02-19 02:10:26 -0800

    some navigations

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 'use strict';
+$(document).ready(function() {
 
+  var $mainButton = $("#loginButton"),
+      $closeButton = $(".close-button"),
+      $buttonWrapper = $(".button-wrapper"),
+      $layer = $(".layered-content");
+
+  $mainButton.on("click", function(){
+      $buttonWrapper.addClass("clicked").delay(1500).queue(function(){
+          $layer.addClass("active");
+      });
+  });
+
+  $closeButton.on("click", function(){
+      $buttonWrapper.removeClass("clicked");
+      $layer.removeClass("active");
+  });
+
+
+
+});
 let app = angular.module('mainApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -20,8 +40,11 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 
 
-app.controller('loginCtrl', function ($scope) {
+app.controller('loginCtrl', function ($scope, $state) {
   $scope.clicked = false;
+  $scope.signIn = function () {
+    $state.go('main')
+  }
 })
 
 app.controller('mainCtrl', function () {
