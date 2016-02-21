057495363258e0c4baf7a3c9b055f196378a727d
Samuel Robertson <samuel.robertson93@gmail.com>
2016-02-20 17:38:24 -0800

    sticky navbar works

---
+++ database.js
@@ -0,0 +1,66 @@
+{
+        "calendar": {
+                "date": "2015-11-03T08:00:00+0000",
+                "events": [
+                        {
+                                "title": "Early Meeting",
+                                "alert": "15 Min",
+                                "start_time": "2015-11-03T15:30:00+0000",
+                                "end_time": "2015-11-03T16:30:00+0000",
+                                "location": "Conference Room 1",
+                                "participants": [
+                                        "Jane Doe",
+                                        "John Smith",
+                                        "James Brown"
+                                ]
+                        },
+                        {
+                                "title": "Group Meeting",
+                                "alert": "15 Min",
+                                "start_time": "2015-11-03T17:30:00+0000",
+                                "end_time": "2015-11-03T18:00:00+0000",
+                                "location": "Conference Room 2",
+                                "participants": [
+                                        "Jane Doe",
+                                        "John Smith",
+                                        "James Brown",
+                                        "John Doe",
+                                        "Robert Smith",
+                                        "John Brown",
+                                        "Jane Smith",
+                                        "Jane Brown"
+                                ]
+                        },
+                        {
+                                "title": "Lunch!!!",
+                                "alert": "15 Min",
+                                "start_time": "2015-11-03T20:00:00+0000",
+                                "end_time": "2015-11-03T21:00:00+0000",
+                                "location": "Steak House",
+                                "participants": [
+                                        "Jane Doe"
+                                ]
+                        },
+                        {
+                                "title": "Afternoon Meeting",
+                                "alert": "15 Min",
+                                "start_time": "2015-11-03T22:00:00+0000",
+                                "end_time": "2015-11-03T23:00:00+0000",
+                                "location": "Conference Room 3",
+                                "participants": [
+                                        "Jane Doe",
+                                        "John Smith"
+                                ]
+                        },
+                        {
+                                "title": "Go Home",
+                                "alert": "15 Min",
+                                "start_time": "2015-11-04T01:00:00+0000",
+                                "location": "Home",
+                                "participants": [
+                                        "My Car"
+                                ]
+                        }
+                ]
+        }
+}
--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 'use strict';
-$(document).ready(function() {
 
-  var $mainButton = $("#loginButton"),
-      $closeButton = $(".close-button"),
-      $buttonWrapper = $(".button-wrapper"),
-      $layer = $(".layered-content");
-
-  $mainButton.on("click", function(){
-      $buttonWrapper.addClass("clicked").delay(1500).queue(function(){
-          $layer.addClass("active");
-      });
-  });
-
-  $closeButton.on("click", function(){
-      $buttonWrapper.removeClass("clicked");
-      $layer.removeClass("active");
-  });
-
-
-
-});
 let app = angular.module('mainApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -38,15 +18,41 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/login');
 });
 
+app.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
 
+        angular.element($window).bind("scroll", function() {
+            if (this.pageYOffset >= 150) {
+                 scope.boolChangeClass = true;
+             } else {
+                 scope.boolChangeClass = false;
+             }
+            scope.$apply();
+        });
+    };
+});
 
 app.controller('loginCtrl', function ($scope, $state) {
   $scope.clicked = false;
   $scope.signIn = function () {
     $state.go('main')
   }
+
 })
 
-app.controller('mainCtrl', function () {
+app.controller('mainCtrl', function ($scope, getData) {
 
+  getData.getCalendar().then(function(resp) {
+    console.log(resp);
+    $scope.date = resp.data.calendar.date;
+    $scope.events = resp.data.calendar.events
+  })
+
+})
+
+
+app.service('getData', function ($http) {
+  this.getCalendar = function () {
+    return $http.get('./assets/data/calendar.json')
+  }
 })
